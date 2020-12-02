4e24a10b80bf4bd04f46e6f6b64a42f5a9f19248
YuhanShi53 <yuhanshi53@outlook.com>
2020-12-02 14:19:18 +0800

    Unsolved leetcode_218

---
+++ solutions/leetcode_218.py
@@ -0,0 +1,13 @@
+""" Leetcode 218 - The skyline problem
+
+https://leetcode.com/problems/the-skyline-problem/
+
+"""
+
+from typing import List
+
+
+class Solution1:
+    """ 1. MINE """
+
+    def get_sky_line(self, building: List[List[int]]) -> List[List[int]]:
