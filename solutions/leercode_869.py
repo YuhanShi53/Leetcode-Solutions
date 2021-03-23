c23ac6fb85d164e09af43e3915b5e9951b6522b6
YuhanShi53 <yuhanshi53@outlook.com>
2021-03-23 19:27:57 +0800

    add solution for Leetcode 869

---
+++ solutions/leercode_869.py
@@ -0,0 +1,11 @@
+""" Leetcode 869 - 
+
+https://leetcode.com/problems/reordered-power-of-2/
+
+"""
+
+class Solution1:
+    """ 1. MINE """
+
+    def reordered_power_of_2(self, N: int) -> bool:
+        
