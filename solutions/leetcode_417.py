ef73b723020ed0e324fa3b12f75b5d7713940719
YuhanShi53 <yuhanshi53@outlook.com>
2021-03-26 23:25:11 +0800

    add solution for Leetcode 417

---
+++ solutions/leetcode_417.py
@@ -0,0 +1,13 @@
+""" Leetcode 417 - 
+
+https://leetcode.com/problems/pacific-atlantic-water-flow/
+
+"""
+
+from typing import List
+
+
+class Solution1:
+    """ 1. MINE """
+
+    def pacific_atlantic(self, matrix: List[List[int]]) -> List[List[int]]:
